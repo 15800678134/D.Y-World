5fe45e53571bb40fccf1231baf41d92fb1f0b085
Your Name <you@example.com>
2017-10-05 22:41:51 +0800

    新增node环境 本地测试环境

---
+++ nodeServer/servers.js
@@ -0,0 +1,32 @@
+var http = require("http");//引入http模块
+var fs = require("fs");
+
+var documentRoot = "C:/Users/longJ/Desktop";//此处配置localhost:端口所对应的地址
+
+//开启服务 打开端口
+var server = http.createServer(function(req,res){
+
+	console.log("已经链接");
+
+	var url = req.url;
+
+	var file = documentRoot + url;
+
+	fs.readFile(file,function(err,data){
+		if(err){
+			res.writeHeader(404,{
+                'content-type' : 'text/html;charset="utf-8"'
+            });
+            res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
+            res.end();
+		}else{
+			res.writeHeader(200,{
+                'content-type' : 'text/html;charset="utf-8"'
+            });
+            res.write(data);
+            res.end();
+		}
+	})
+}).listen(8080);
+
+console.log("服务器已经开启");
